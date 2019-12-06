b45cf5b9941e1484c259c4f761414b2651fb928f
Filipe Rocha <contato.filiperochs@gmail.com>
2019-12-06 19:55:09 +0000

    novas2

--- br/com/trab1pc2/produto/Lote.java
+++ br/com/trab1pc2/produto/Lote.java
@@ -1,5 +1,12 @@
-package br.com.trab1pc2.produto;
+package br.com.trab1pc2.sistema;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Lote{
-    
+  private int id;
+  private Date validade;
+  private int quatidade;
+   
 }
